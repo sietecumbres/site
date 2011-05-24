b3e09c5f830b406408ccd86a834e616fefca7ea2
Alejandro Toro <alejandrotoro@espiral.co>
2011-05-23 22:43:37 -0500

    Primeras pruebas con las imagenes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-	
+	$(document).mousemove(function(e){
+		$('#status').html(e.pageX +', '+ e.pageY);
+		
+		// Marco
+		if(e.pageY >= 596 && e.pageX >= 668 && e.pageX <= 738 && e.pageY <= 675)
+		{
+			$('#foto_marco').removeClass('front');
+			$('#foto_marco').addClass('left');
+		} 
+	});
 });
 
 function chooseButton(element, style){ 
