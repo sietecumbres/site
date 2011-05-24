68bcc1a5edae47ef0ab4d5ec0067e1fa4974f5c2
Alejandro Toro <alejandrotoro@espiral.co>
2011-05-23 23:46:15 -0500

    Seguimiento de cursor

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,239 @@
 $(document).ready(function(){
 	$(document).mousemove(function(e){
-		$('#status').html(e.pageX +', '+ e.pageY);
 		
 		// Marco
-		if(e.pageY >= 596 && e.pageX >= 668 && e.pageX <= 738 && e.pageY <= 675)
+		var marco = $("#foto_marco");
+		var mp = marco.position();
+		
+		var left = mp.left + 20;
+		var right = mp.left + 91;
+		var top = mp.top + 6;
+		var below = mp.top + 86;
+		
+		if(e.pageY >= top && e.pageX >= left && e.pageX <= right && e.pageY <= below)
 		{
-			$('#foto_marco').removeClass('front');
-			$('#foto_marco').addClass('left');
-		} 
+			$('#foto_marco').removeClass('left');
+			$('#foto_marco').removeClass('right');
+			$('#foto_marco').removeClass('up');
+			$('#foto_marco').removeClass('down');
+			$('#foto_marco').addClass('front');
+		} else 
+		{
+			if(e.pageY <= below && e.pageY >= top && e.pageX <= left)
+			{
+				$('#foto_marco').removeClass('front');
+				$('#foto_marco').removeClass('right');
+				$('#foto_marco').removeClass('up');
+				$('#foto_marco').removeClass('down');
+				$('#foto_marco').addClass('left');
+			} else
+			{
+				if(e.pageY <= below && e.pageY >= top && e.pageX >= right)
+				{
+					$('#foto_marco').removeClass('front');
+					$('#foto_marco').removeClass('left');
+					$('#foto_marco').removeClass('up');
+					$('#foto_marco').removeClass('down');
+					$('#foto_marco').addClass('right');
+				} else
+				{
+					if(e.pageX <= right && e.pageX >= left && e.pageY <= top)
+					{
+						$('#foto_marco').removeClass('front');
+						$('#foto_marco').removeClass('left');
+						$('#foto_marco').removeClass('right');
+						$('#foto_marco').removeClass('down');
+						$('#foto_marco').addClass('up');
+					} else
+					{
+						if(e.pageX <= right && e.pageX >= left && e.pageY >= below)
+						{
+							$('#foto_marco').removeClass('front');
+							$('#foto_marco').removeClass('left');
+							$('#foto_marco').removeClass('right');
+							$('#foto_marco').removeClass('up');
+							$('#foto_marco').addClass('down');
+						}
+					}
+				}
+			}
+		}
+		
+		// Juan
+		var juan = $("#foto_juan");
+		var jp = juan.position();
+		
+		var left = jp.left + 20;
+		var right = jp.left + 91;
+		var top = jp.top + 6;
+		var below = jp.top + 86;
+		
+		if(e.pageY >= top && e.pageX >= left && e.pageX <= right && e.pageY <= below)
+		{
+			$('#foto_juan').removeClass('left');
+			$('#foto_juan').removeClass('right');
+			$('#foto_juan').removeClass('up');
+			$('#foto_juan').removeClass('down');
+			$('#foto_juan').addClass('front');
+		} else 
+		{
+			if(e.pageY <= below && e.pageY >= top && e.pageX <= left)
+			{
+				$('#foto_juan').removeClass('front');
+				$('#foto_juan').removeClass('right');
+				$('#foto_juan').removeClass('up');
+				$('#foto_juan').removeClass('down');
+				$('#foto_juan').addClass('left');
+			} else
+			{
+				if(e.pageY <= below && e.pageY >= top && e.pageX >= right)
+				{
+					$('#foto_juan').removeClass('front');
+					$('#foto_juan').removeClass('left');
+					$('#foto_juan').removeClass('up');
+					$('#foto_juan').removeClass('down');
+					$('#foto_juan').addClass('right');
+				} else
+				{
+					if(e.pageX <= right && e.pageX >= left && e.pageY <= top)
+					{
+						$('#foto_juan').removeClass('front');
+						$('#foto_juan').removeClass('left');
+						$('#foto_juan').removeClass('right');
+						$('#foto_juan').removeClass('down');
+						$('#foto_juan').addClass('up');
+					} else
+					{
+						if(e.pageX <= right && e.pageX >= left && e.pageY >= below)
+						{
+							$('#foto_juan').removeClass('front');
+							$('#foto_juan').removeClass('left');
+							$('#foto_juan').removeClass('right');
+							$('#foto_juan').removeClass('up');
+							$('#foto_juan').addClass('down');
+						}
+					}
+				}
+			}
+		}
+		
+		// Gordo
+		var gordo = $("#foto_gordo");
+		var gp = gordo.position();
+		
+		var left = gp.left + 20;
+		var right = gp.left + 91;
+		var top = gp.top + 6;
+		var below = gp.top + 86;
+		
+		if(e.pageY >= top && e.pageX >= left && e.pageX <= right && e.pageY <= below)
+		{
+			$('#foto_gordo').removeClass('left');
+			$('#foto_gordo').removeClass('right');
+			$('#foto_gordo').removeClass('up');
+			$('#foto_gordo').removeClass('down');
+			$('#foto_gordo').addClass('front');
+		} else 
+		{
+			if(e.pageY <= below && e.pageY >= top && e.pageX <= left)
+			{
+				$('#foto_gordo').removeClass('front');
+				$('#foto_gordo').removeClass('right');
+				$('#foto_gordo').removeClass('up');
+				$('#foto_gordo').removeClass('down');
+				$('#foto_gordo').addClass('left');
+			} else
+			{
+				if(e.pageY <= below && e.pageY >= top && e.pageX >= right)
+				{
+					$('#foto_gordo').removeClass('front');
+					$('#foto_gordo').removeClass('left');
+					$('#foto_gordo').removeClass('up');
+					$('#foto_gordo').removeClass('down');
+					$('#foto_gordo').addClass('right');
+				} else
+				{
+					if(e.pageX <= right && e.pageX >= left && e.pageY <= top)
+					{
+						$('#foto_gordo').removeClass('front');
+						$('#foto_gordo').removeClass('left');
+						$('#foto_gordo').removeClass('right');
+						$('#foto_gordo').removeClass('down');
+						$('#foto_gordo').addClass('up');
+					} else
+					{
+						if(e.pageX <= right && e.pageX >= left && e.pageY >= below)
+						{
+							$('#foto_gordo').removeClass('front');
+							$('#foto_gordo').removeClass('left');
+							$('#foto_gordo').removeClass('right');
+							$('#foto_gordo').removeClass('up');
+							$('#foto_gordo').addClass('down');
+						}
+					}
+				}
+			}
+		}
+		
+		// Alejo
+		var alejo = $("#foto_alejo");
+		var ap = alejo.position();
+		
+		var left = ap.left + 20;
+		var right = ap.left + 91;
+		var top = ap.top + 6;
+		var below = ap.top + 86;
+		
+		if(e.pageY >= top && e.pageX >= left && e.pageX <= right && e.pageY <= below)
+		{
+			$('#foto_alejo').removeClass('left');
+			$('#foto_alejo').removeClass('right');
+			$('#foto_alejo').removeClass('up');
+			$('#foto_alejo').removeClass('down');
+			$('#foto_alejo').addClass('front');
+		} else 
+		{
+			if(e.pageY <= below && e.pageY >= top && e.pageX <= left)
+			{
+				$('#foto_alejo').removeClass('front');
+				$('#foto_alejo').removeClass('right');
+				$('#foto_alejo').removeClass('up');
+				$('#foto_alejo').removeClass('down');
+				$('#foto_alejo').addClass('left');
+			} else
+			{
+				if(e.pageY <= below && e.pageY >= top && e.pageX >= right)
+				{
+					$('#foto_alejo').removeClass('front');
+					$('#foto_alejo').removeClass('left');
+					$('#foto_alejo').removeClass('up');
+					$('#foto_alejo').removeClass('down');
+					$('#foto_alejo').addClass('right');
+				} else
+				{
+					if(e.pageX <= right && e.pageX >= left && e.pageY <= top)
+					{
+						$('#foto_alejo').removeClass('front');
+						$('#foto_alejo').removeClass('left');
+						$('#foto_alejo').removeClass('right');
+						$('#foto_alejo').removeClass('down');
+						$('#foto_alejo').addClass('up');
+					} else
+					{
+						if(e.pageX <= right && e.pageX >= left && e.pageY >= below)
+						{
+							$('#foto_alejo').removeClass('front');
+							$('#foto_alejo').removeClass('left');
+							$('#foto_alejo').removeClass('right');
+							$('#foto_alejo').removeClass('up');
+							$('#foto_alejo').addClass('down');
+						}
+					}
+				}
+			}
+		}
+		
+				
 	});
 });
 
